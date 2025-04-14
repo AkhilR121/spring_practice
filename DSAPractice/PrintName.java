831e7afa453a8178aa0719b4fc7116279aaeb69f
Akhil Vinnakota <akhil.vinnakota@technoidentity.com>
2025-04-15 00:38:55 +0530

    add recurrsion ex2

--- DSAPractice/PrintName.java
+++ DSAPractice/PrintName.java
@@ -5,17 +5,23 @@ public class PrintName {
         Scanner sc = new Scanner(System.in);
         
         RecursionPrintName obj = new RecursionPrintName();
-        int i = 1;
+        // int i = 1;
         System.out.println("Give any nunber: ");
         int n = sc.nextInt();
-        obj.recursiveFunction(i, n);
+        obj.recursiveFunction(n, n);
     }
 }
 
 class RecursionPrintName{
+    // public void recursiveFunction(int i, int n){
+    //     if(i > n) return;
+    //     System.out.println("Name: " + "Hello");
+    //     recursiveFunction(i+1, n);
+    // }
+
     public void recursiveFunction(int i, int n){
-        if(i > n) return;
-        System.out.println("Name: " + "Hello");
-        recursiveFunction(i+1, n);
+        if(i < 1) return;
+        recursiveFunction(i-1, n);
+        System.out.println("Number: " + i);
     }
 }
